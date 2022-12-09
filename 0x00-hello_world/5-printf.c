db7a35861c4429b3bf1bb0b3024df2a7b74a20b5
Bramuel <wecbrams@gmail.com>
2022-12-09 01:45:34 -0800

    Initial commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -serves as the entry point for the program
+ * Return _Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	return (0);
+}
+
