c7262f62f36a83da9759b6ac5c5cb1b242790f2b
Bramuel <wecbrams@gmail.com>
2022-12-09 01:56:20 -0800

    initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /*
  * main -serves as the entry point for the program
@@ -10,3 +10,4 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
