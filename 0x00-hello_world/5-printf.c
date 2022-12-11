24279889e49639a186a9af8476ebe318eb51af33
Bramuel <wecbrams@gmail.com>
2022-12-11 04:24:08 -0800

    Initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /*
  * main - prints the phrases with proper grammar,
  * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwis
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
 
