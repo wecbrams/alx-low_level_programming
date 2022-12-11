1bc30c2452f736d6b185dcdf9c90f9c537d74ba0
Bramuel <wecbrams@gmail.com>
2022-12-11 13:59:34 -0800

    Initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 
 /*
- * main - main block
- * Description: Prints the phrases with proper grammar,
+ * main - Prints the phrases with proper grammar,
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
@@ -10,6 +9,6 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
 
