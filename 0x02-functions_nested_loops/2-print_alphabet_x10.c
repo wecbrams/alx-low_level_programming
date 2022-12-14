7e027e4cf8b269f33e4800e03d97f87b93990a5b
Bramuel <wecbrams@gmail.com>
2022-12-14 04:20:18 -0800

    Initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char n;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (n = 0; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n);
+	}
+}
+
