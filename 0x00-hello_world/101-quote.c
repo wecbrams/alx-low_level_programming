d3618a86833b2456331d525b5f6cd74134b99f5b
Bramuel <wecbrams@gmail.com>
2023-03-19 02:45:32 -0700

    heollo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,8 @@
 
 /**
  * main - Entry point
- * 
  * Description: print a quote using write function
- * ssize_t write (int fd, const void *buf, size_t count);
+ * ssize_t write(int fd, const void *buf, size_t count);
  *
  * Return: This time we return an error 1.
  */
@@ -14,7 +13,7 @@ int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quo, 59);
+	write (1, quo, 59);
 	return(1);
 }
 
