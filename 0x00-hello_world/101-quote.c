790542915dad5094dacc0f92bf59e7b3b696720d
Bramuel <wecbrams@gmail.com>
2023-03-19 02:40:59 -0700

    heollo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - Entry point
  * 
  * Description: print a quote using write function
- * 	ssize_t write(int fd, const void *buf, size_t count);
+ * ssize_t write (int fd, const void *buf, size_t count);
  *
  * Return: This time we return an error 1.
  */
