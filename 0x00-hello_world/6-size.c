d3c4f47e0d1bfd5fb5f65f56dac580f79826915d
Bramuel <wecbrams@gmail.com>
2023-03-19 02:53:10 -0700

    heollo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a  float: %d byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
 
