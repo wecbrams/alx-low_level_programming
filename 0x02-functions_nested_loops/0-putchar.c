fc08ee6e9a0f3b17697889da4143098147da4e64
Bramuel <wecbrams@gmail.com>
2023-03-26 07:30:42 -0700

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	char str[]="_putchar";
+	char str[] = "_putchar";
 	int ch = 0;
-	void _putchar();
+	void _putchar(void);
 
 	while (str[ch] != '\0')
 	{
 		_putchar(str[ch]);
 		ch++;
-	}	
+	}
 	_putchar('\n');
 
 	return (0);
