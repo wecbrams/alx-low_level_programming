1e19d29fb44232d7e4dfb0960a0621845f892f62
wecbrams <97198443+wecbrams@users.noreply.github.com>
2023-10-23 21:10:27 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,30 +1,22 @@
 #include "main.h"
-
 /**
- * _strpbrk - prints s1
- * @s: source string
- * @ccept: searching string
- * Return: new string
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+	int k;
 
-	for (i = 0; *(s + i); i++)
+	while (*s)
 	{
-		for (j = 0; *(accept + j); j++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (*(s + i) == *(accept + j))
-			{
-				break;
-			}
+		if (*s == accept[k])
+		return (s);
 		}
-		if (*(accept + j) != '\0')
-		{
-			return (s + i);
-		}
+	s++;
 	}
-	return (0);
+	return ('\0');
 }
-
